acf53e6760e8a91ea92f12526cd9635b5ccdeb9f
Murat Karaca <murat.karaca@commencis.com>
2025-05-25 14:43:23 +0300

    fix: fix queues

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,8 @@
 
 int main()
 {
-    moodycamel::ConcurrentQueue<std::string> rabbitmq_queue;
-    moodycamel::ConcurrentQueue<std::string> kafka_queue;
-    moodycamel::ConcurrentQueue<std::string> persistence_queue;
+    moodycamel::ConcurrentQueue<std::string> execution_report_queue;
+    moodycamel::ConcurrentQueue<std::string> market_data_update_queue;
 
     return 0;
 }
