fd0685a6e41cfb59a8e09cd33ef180b1f28e9ea6
Murat Karaca <murat.karaca@commencis.com>
2025-05-27 14:37:06 +0300

    feat: add rabbit mq publish

---
+++ include/rabbit_mq_connector.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+extern "C"
+{
+#include <amqp.h>
+#include <amqp_tcp_socket.h>
+}
+
+class RabbitMQConnector
+{
+private:
+    amqp_connection_state_t conn;
+    amqp_socket_t *socket;
+
+public:
+    ~RabbitMQConnector();
+
+    bool connect(const std::string &hostname, int port,
+                 const std::string &username, const std::string &password);
+
+    bool declareQueue(const std::string &queueName);
+
+    bool publishMessage(const std::string &queueName, const std::string &message);
+
+    void disconnect();
+};
