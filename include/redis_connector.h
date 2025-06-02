dee259dbb20de0aa278acf52e8f563978ba3f52e
Murat Karaca <murat.karaca@commencis.com>
2025-06-02 17:56:02 +0300

    chore: add description and fix return values

--- include/redis_connector.h
+++ include/redis_connector.h
@@ -20,9 +20,32 @@ public:
     RedisConnector();
     ~RedisConnector() noexcept;
 
+    /**
+     * @brief Append a publish command to the Redis pipeline.
+     * @param channel The channel to publish the message to.
+     * @param message The message to publish.
+     * @return True if the command was appended successfully, false otherwise.
+     */
     bool append_publish_command(const std::string &channel, const std::string &message);
+
+    /**
+     * @brief Append a timeseries command to the Redis pipeline.
+     * @param key The key to add the timeseries data to.
+     * @param timestamp The timestamp of the data.
+     * @param value The value of the data.
+     * @return True if the command was appended successfully, false otherwise.
+     */
     bool append_timeseries_command(const std::string &key, int64_t timestamp, double value);
 
+    /**
+     * @brief Get a reply from the Redis pipeline. Triggers network call if called after appending commands.
+     * @return A pointer to the reply. nullptr if there is no reply or on error.
+     */
     redisReply *get_reply();
+
+    /**
+     * @brief Free a reply from memory.
+     * @param reply The reply to free.
+     */
     void free_reply(redisReply *reply);
 };
--- src/redis_connector.cpp
+++ src/redis_connector.cpp
@@ -15,12 +15,14 @@ RedisConnector::~RedisConnector()
 
 bool RedisConnector::append_publish_command(const std::string &channel, const std::string &message)
 {
-    return redisAppendCommand(conn, "PUBLISH %s %s", channel.c_str(), message.c_str());
+    int result = redisAppendCommand(conn, "PUBLISH %s %s", channel.c_str(), message.c_str());
+    return result == REDIS_OK;
 }
 
 bool RedisConnector::append_timeseries_command(const std::string &key, int64_t timestamp, double value)
 {
-    return redisAppendCommand(conn, "TS.ADD %s %lld %f", key.c_str(), timestamp, value);
+    int result = redisAppendCommand(conn, "TS.ADD %s %lld %f", key.c_str(), timestamp, value);
+    return result == REDIS_OK;
 }
 
 redisReply *RedisConnector::get_reply()
